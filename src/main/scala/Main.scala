909601c48de41e00a9582e1eedd1a71ec9035531
EOussama <oussama_gtacj@live.fr>
2023-02-15 11:44:43 +0100

    Added sbt

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,15 @@
+
+object Main extends App {
+        
+  // Ask the user to enter their first name
+  print("Enter your first name: ")
+  val firstName = scala.io.StdIn.readLine()
+
+  // Ask the user to enter their last name
+  print("Enter your last name: ")
+  val lastName = scala.io.StdIn.readLine()
+
+  // Ask the user to enter their age
+  print("Enter your age: ")
+  val age = scala.io.StdIn.readInt()
+}
